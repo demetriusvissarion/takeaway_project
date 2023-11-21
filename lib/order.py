846a7cc965efcd402df8d49c4a3b3c9ea35730f3
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-11-21 12:23:57 +0000

    finished order class

--- lib/order.py
+++ lib/order.py
@@ -1,2 +1,28 @@
 class Order:
-    pass
+    def __init__(self):
+        self.menu = ['Fish and chips - £12', 'English breakfast - £10']
+
+    def show_menu(self):
+        return self.menu
+    
+    def build_numbered_menu(self):
+        self.numbered_menu = []
+        counter = 1
+        for item in self.menu:
+            self.numbered_menu.append(str(counter) + ': ' + item)
+            counter += 1
+        return self.numbered_menu
+    
+    def build_order(self):
+        print(self.numbered_menu)
+        self.itemised_receipt = []
+        self.food_and_quantity = input('Enter menu item number and quantity, example: 13 for three portions of "Fish and chips":')
+        # print('int(self.food_and_quantity[0]): ', int(self.food_and_quantity[0]))
+        # print('int(self.food_and_quantity[1]): ', int(self.food_and_quantity[1]))
+        if int(self.food_and_quantity[0]) not in range(1, 3):
+            raise Exception("There is no item menu with that number")
+        elif int(self.food_and_quantity[1]) not in range(1, 6):
+            raise Exception("Wrong quantity, you can only order minimum 1 and maximum 5 of each item")
+        else: 
+            self.itemised_receipt.append(f'{str(self.food_and_quantity[1])}x' + self.numbered_menu[int(self.food_and_quantity[0]) - 1][2:-3] + f'£{int(self.food_and_quantity[1]) * int(self.numbered_menu[int(self.food_and_quantity[0]) - 1][-2:])}')
+            return self.itemised_receipt
--- tests/test_order.py
+++ tests/test_order.py
@@ -13,7 +13,7 @@ def test_if_shows_menu():
 
 """
 Given an instance of the order
-#build_numbered_menu displays the menu with a number for each dish
+#build_numbered_menu returns the menu with a number for each dish
 """
 def test_if_builds_numbered_menu():
     order = Order()
@@ -21,10 +21,46 @@ def test_if_builds_numbered_menu():
 
 
 """
-Given an instance of the order
-#order_food expects two input numbers (food & qty) and returns the ordered food
+Given an instance of the order and good input
+#build_order expects two input numbers (food & qty) and returns the ordered food
 """
-def test_if_orders_food():
+def test_if_builds_order_input13():
     order = Order()
-    assert order.build_numbered_menu() == ['1: Fish and chips - £12', '2: English breakfast - £10']
-    assert order.order_food(3) == ['3x Fish and chips - £12']
+    order.build_numbered_menu()
+    assert order.build_order() == ['3x Fish and chips - £36'] # input 13
+
+
+"""
+Given an instance of the order and good input
+#build_order expects two input numbers (food & qty) and returns the ordered food
+"""
+def test_if_builds_order_input24():
+    order = Order()
+    order.build_numbered_menu()
+    assert order.build_order() == ['4x English breakfast - £40'] # input 24
+
+
+"""
+Given an instance of the order and wrong item input
+#build_order expects two input numbers (food & qty) and returns the ordered food
+"""
+def test_if_builds_order_wrong_item_input():
+    order = Order()
+    order.build_numbered_menu()
+    with pytest.raises(Exception) as e: 
+        order.build_order() # input 01 or any wrong item input
+    error_message = str(e.value)
+    assert error_message == "There is no item menu with that number"
+
+
+"""
+Given an instance of the order and wrong quantity input
+#build_order expects two input numbers (food & qty) and returns the ordered food
+"""
+def test_if_builds_order_wrong_qty_input():
+    order = Order()
+    order.build_numbered_menu()
+    with pytest.raises(Exception) as e: 
+        order.build_order() # input 10 or any wrong quantity input
+    error_message = str(e.value)
+    assert error_message == "Wrong quantity, you can only order minimum 1 and maximum 5 of each item"
